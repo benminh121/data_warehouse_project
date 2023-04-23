b9c73536234e12a96c6fdb8c4b764f9aad47dd0f
Minh Nguyen <15643@student.vgu.edu.vn>
2023-04-23 14:34:59 +0700

    layer dim product

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,5 +1,20 @@
-SELECT 
-  cast(stock_item_id as integer) as product_key,
-  cast(stock_item_name as string) as product_name,
-  cast(brand as string) as brand_name,
-FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+-- 1st layer, select all resource from dim_product
+WITH dim_product__source AS(
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+)
+
+-- 2nd layer, select specific fields and perform CAST
+, dim_product__rename_recast AS (
+  SELECT 
+      CAST(stock_item_id AS INT) AS product_key
+    , CAST(stock_item_name AS STRING) AS product_name
+    , CAST(brand AS STRING) AS brand_name
+  FROM dim_product__source
+)
+
+select
+  product_key
+  , product_name
+  , brand_name
+from dim_product__rename_recast
