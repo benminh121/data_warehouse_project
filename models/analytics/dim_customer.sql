e70d86eca6ac32ea1239ed149de9eeb365fb0c75
Minh Nguyen <15643@student.vgu.edu.vn>
2023-04-23 22:26:10 +0700

    is credit hold

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -10,8 +10,22 @@ WITH dim_customer__source AS (
     , CAST(customer_name AS STRING) AS customer_name
     , CAST(customer_category_id AS INT) AS customer_category_key
     , CAST(buying_group_id AS INT) AS buying_group_key
+    , CAST(is_on_credit_hold AS BOOLEAN) AS is_on_credit_hold_boolean
   FROM dim_customer__source
 )
+
+, dim_customer__convert_boolean AS (
+  SELECT
+    *
+    , CASE
+        WHEN is_on_credit_hold_boolean IS TRUE THEN 'On Credit Hold'
+        WHEN is_on_credit_hold_boolean IS FALSE THEN 'Not On Credit Hold'
+        WHEN is_on_credit_hold_boolean IS NULL THEN 'Undefined'
+        ELSE 'Invalid' END
+      AS is_on_credit_hold -- Separate between Undefined (null) and Invalid data
+  FROM dim_customer__rename_recast
+)
+
 SELECT 
   dim_customer.customer_key
   , dim_customer.customer_name
@@ -19,7 +33,8 @@ SELECT
   , dim_customer_category.customer_category_name
   , dim_customer.buying_group_key
   , dim_buying_group.buying_group_name
-FROM dim_customer__rename_recast as dim_customer
+  , dim_customer.is_on_credit_hold
+FROM dim_customer__convert_boolean as dim_customer
 LEFT JOIN {{ ref('stg_dim_customer_category') }} AS dim_customer_category
   ON dim_customer.customer_category_key = dim_customer_category.customer_category_key
 
