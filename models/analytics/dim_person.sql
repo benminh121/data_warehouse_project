ce4844ab296841149ce8ffa1721de03e24122869
Minh Nguyen <15643@student.vgu.edu.vn>
2023-06-15 11:06:52 +0700

    dim_person

--- models/analytics/dim_person.sql
+++ models/analytics/dim_person.sql
@@ -8,25 +8,62 @@ WITH dim_person__source AS(
   SELECT
     CAST(person_id AS INT) AS person_key
     , CAST(full_name AS STRING) AS full_name
+    , CAST(preferred_name AS STRING) AS preferred_name
+    , CAST(is_employee AS BOOLEAN) AS is_employee_boolean
+    , CAST(is_salesperson AS BOOLEAN) AS is_salesperson_boolean
   FROM dim_person__source
+)
+
+, dim_person__convert_boolean AS (
+  SELECT
+    person_key
+    , full_name
+    , coalesce(preferred_name, 'Undefined') AS preferred_name
+    , CASE
+        WHEN is_employee_boolean IS TRUE THEN 'Employee'
+        WHEN is_employee_boolean IS FALSE THEN 'Not Employee'
+        WHEN is_employee_boolean IS NULL THEN 'Undefined'
+        ELSE 'Invalid'
+      END AS is_employee
+    , CASE
+        WHEN is_salesperson_boolean IS TRUE THEN 'Salesperson'
+        WHEN is_salesperson_boolean IS FALSE THEN 'Not Salesperson'
+        WHEN is_salesperson_boolean IS NULL THEN 'Undefined'
+        ELSE 'Invalid'
+      END AS is_salesperson
+  FROM dim_person__rename_recast
 )
 
 , dim_person__add_undefined_record AS (
   SELECT
     person_key
     , full_name
-  FROM dim_person__rename_recast
+    , preferred_name
+    , is_employee
+    , is_salesperson
+  FROM dim_person__convert_boolean
+  
   UNION ALL
   SELECT
     0 AS person_key
     , 'Undefined' AS full_name
+    , 'Undefined' AS preferred_name
+    , 'Undefined' AS is_employee
+    , 'Undefined' AS is_salesperson
+
   UNION ALL
   SELECT
     -1 AS person_key
-    , 'Error' AS full_name
+    , 'Invalid' AS full_name
+    , 'Invalid' AS preferred_name
+    , 'Invalid' AS is_employee
+    , 'Invalid' AS is_salesperson
 )
 
 SELECT
-  person_key
+    person_key
   , full_name
-FROM dim_person__add_undefined
+  , preferred_name
+  , is_employee
+  , is_salesperson
+FROM dim_person__add_undefined_record
